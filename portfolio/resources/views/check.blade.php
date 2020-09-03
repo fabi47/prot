7ff3ff394c603855d4cfa63d81a05c7750864fc2
fabi47 <fabi.47jsc@icloud.com>
2020-09-03 21:12:35 +0900

    portfolio

---
+++ portfolio/resources/views/check.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="ja">
+    <head>
+        <meta charset="UTF-8" />
+        <title>check</title>
+        <link rel="stylesheet" href="checkn.css" />
+        <link rel="stylesheet" href="all.css" />
+    </head>
+    <body>
+        <header>header</header>
+
+        <main>
+            <p>ここに確認画面</p>
+        </main>
+
+        <footer>footer</footer>
+    </body>
+</html>
---
+++ portfolio/resources/views/date.blade.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="ja">
+    <head>
+        <meta charset="UTF-8" />
+        <title>daet</title>
+        <link rel="stylesheet" href="date.css" />
+        <link rel="stylesheet" href="all.css" />
+      </head>
+    <body>
+        <header>header</header>
+            <main>
+                <img src="date_back.jpg" title="date_back">
+                <div class="date">
+                    <P>ここにカレンダー</p>
+                </div>
+            </main>
+        <footer>footer</footer>
+    </body>
+</html>
