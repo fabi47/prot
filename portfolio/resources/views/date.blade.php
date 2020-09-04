c93f63380ba043ff56878df790127af4ed11a840
SouzaFabiano <67329184+fabi47@users.noreply.github.com>
2020-09-04 13:45:28 +0900

    Update date.blade.php

--- portfolio/resources/views/date.blade.php
+++ portfolio/resources/views/date.blade.php
@@ -12,6 +12,10 @@
                 <img src="date_back.jpg" title="date_back">
                 <div class="date">
                     <P>ここにカレンダー</p>
+                    <form action="/check" method="post" enctype="multipart/form-data">
+                @csrf
+                <input type="submit" value="next">
+              </form>
                 </div>
             </main>
         <footer>footer</footer>
